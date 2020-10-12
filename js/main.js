dbef27e1b70187580ab1348c5c1274f5eb620f6d
Serezhaua <Kalitin1997>
2020-10-12 17:08:11 +0300

    add block edit css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $('.nav__search').click( function(){
     $('.nav__search-block').toggle();
 });
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+// scroll
+    $(document).ready(function(){
+        $(".menu").on("click","a", function (event) {
+            event.preventDefault();
+            var id  = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    });
